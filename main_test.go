cc7c9c35578481c2d2d3abd6af8b92b7aff45065
Abe Miessler <AbeMiessler@yahoo.com>
2020-09-14 09:49:43 -0700

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -12,7 +12,6 @@ func TestGORM(t *testing.T) {
 	user := User{Name: "jinzhu"}
 
 	DB.Create(&user)
-
 	var result User
 	if err := DB.First(&result, user.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
